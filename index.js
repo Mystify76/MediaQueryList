2fd3909e86f09309b583ac33e899f5a2c67b4d9e
dave-edu <david.gallant@educode.ca>
2019-11-26 15:39:27 -0400

    removed dist and build

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-'use strict';
-module.exports = require("./dist/mediaQueryList");
+class MediaQueryList {
+  constructor(matchMedia) {
+    this._mql = window.matchMedia(matchMedia);
+  };
+
+  get matches() {
+    return this._mql.matches;
+  }
+
+  get media() {
+    return this._mql.media;
+  }
+
+  addListener = (callback) => {
+    try {
+      this._mql.addEventListener('change', callback);
+    } catch (err) {
+      this._mql.addListener(callback);
+    }
+  };
+
+  removeListener = (callback) => {
+    try {
+      this._mql.removeEventListener('change', this.mediaQueryChanged);
+    } catch (err) {
+      this._mql.removeListener(this.mediaQueryChanged);
+    }
+  };
+}
+
+module.exports = MediaQueryList;
